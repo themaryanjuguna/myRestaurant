dc4da0567248e4cdda1fdefcc03b53b180c78b53
themaryanjuguna <themaryanjugunaagmail.com>
2022-05-26 13:49:23 +0300

    Gathering Data from EditText

--- app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
+++ app/src/main/java/com/moringaschool/myrestaurants/MainActivity.java
@@ -1,30 +1,40 @@
 package com.moringaschool.myrestaurants;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mFindRestaurantsButton;
+    private EditText mFindLocationEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        mFindLocationEditText = (EditText) findViewById(R.id.findLocationEditText);
         mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
-
-        mFindRestaurantsButton= (Button) findViewById(R.id.findRestaurantsButton);
-        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener()
+        {
             @Override
             public void onClick(View v) {
+                String location = mFindLocationEditText.getText().toString();
+                Log.d(TAG, location);
                 //do something
-                Toast.makeText(MainActivity.this, "Hello World!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, RestaurantActivity.class);
+                startActivity(intent);
+              /*  String location = mFindLocationEditText.getText().toString();
+                Toast.makeText(MainActivity.this, location, Toast.LENGTH_LONG).show();*/
+
             }
         });
 
